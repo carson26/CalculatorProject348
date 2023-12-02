13c11ac8eab265b1fcee13ab6582293c214670c6
bandrad <143830114+bandrad@users.noreply.github.com>
2023-12-02 09:36:32 -0600

    Update main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -4,7 +4,7 @@
 * main.cpp
 */
 
-#include "Calculator.h"
+
 #include "Calculator.cpp"
 #include <iostream>
 #include <string>
@@ -16,6 +16,29 @@ int main()
 {
     // Here is where the user interface will be implemented for the most part!
 
-    cout << "Hello World!\n";
+    string expression;
+    cout << "--------Calculator---------\n" << endl;
+    cout << "Please Enter a mathematical expression or TYPE 0 to EXIT" << endl;
+
+    cin >> expression;
+
+    if (expression == "0")
+    {
+        //end program
+    }
+    else
+    {
+        Calculator calc(expression);
+        double result = calc.calculate();
+
+        cout << "Result:" << result << endl;
+
+    }
+
+
+   
     
-}
+
+    
+}//end Main
+
